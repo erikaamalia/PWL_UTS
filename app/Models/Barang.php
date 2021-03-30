a46e5a7740fbc928ff9837cfcb47a070231bd4c1
erikaamalia <erikamalia181@gmail.com>
2021-03-30 08:57:41 +0700

    Setting nilai

--- app/Models/Barang.php
+++ app/Models/Barang.php
@@ -2,10 +2,28 @@
 
 namespace App\Models;
 
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\Mahasiswa as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Model; //Model Eloquent
 
-class Barang extends Model
+class Barang extends Model //Definisi Model
 {
-    use HasFactory;
-}
+    protected $table="mahasiswa"; // Eloquent akan membuat model barang menyimpan record di tabel barangs
+    public $timestamps= false; 
+    protected $primaryKey = 'nim'; // Memanggil isi DB Dengan primarykey
+ /**
+ * The attributes that are mass assignable.
+ *
+ * @var array
+ */
+protected $fillable = [
+    'id_barang',
+    'kode_barang',
+    'nama_barang',
+    'kategori_barang',
+    'harga',
+    'qty',
+    ];  
+};
